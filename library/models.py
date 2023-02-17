a5791bd4ff499479d852259813361d48d0cd0225
Inna Kushnir <innakushnir63@gmail.com>
2023-02-17 23:52:15 +0200

    Add models

--- library/models.py
+++ library/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
+class Book(models.Model):
+
+    class CoverChoices(models.TextChoices):
+        HARD = "Hard"
+        SOFT = "Soft"
+
+    title = models.CharField(max_length=63)
+    author =models.ManyToManyField(Author, related_name="books")
+    cover = models.CharField(max_length=10, choices=CoverChoices.choices)
+    inventory = models.IntegerField()
+    daily_fee = models.DecimalField()
+
+class Borrowing(models.Model):
+    borrow_date = models.DateField()
+    expected_return_date = models.DateField(auto_now=False)
+    actual_return_date = models.DateField(auto_now=False)
+    book_id = models.IntegerField()
+    user_id = models.IntegerField()
 
