2ac3712d0b475e9ccd25ce688ee2c7b59863b4c5
Inna Kushnir <innakushnir63@gmail.com>
2023-02-24 12:09:29 +0200

    Book

--- library/models.py
+++ library/models.py
@@ -31,7 +31,7 @@ class Borrowing(models.Model):
     borrow_date = models.DateField(auto_now_add=True)
     expected_return_date = models.DateField(auto_now=False)
     actual_return_date = models.DateField(auto_now=False, null=True, blank=True)
-    book_id = models.ForeignKey(
+    book = models.ForeignKey(
         Book, on_delete=models.CASCADE, related_name="borrowings"
     )
     user = models.ForeignKey(
