1bd3829775ecd2efce46794fc4e28f4037bcf8e6
Inna Kushnir <innakushnir63@gmail.com>
2023-05-06 17:11:51 +0300

    Solution

--- library_service/settings.py
+++ library_service/settings.py
@@ -95,7 +95,7 @@ DATABASES = {
         "ENGINE": "django.db.backends.postgresql",
         "NAME": os.environ["POSTGRES_DB"],
         "USER": os.environ["POSTGRES_USER"],
-        "PASSWORD": "library_service_password",
+        "PASSWORD": os.environ["POSTGRES_PASSWORD"],
         "HOST": os.environ["POSTGRES_HOST"],
         "PORT": os.environ["POSTGRES_PORT"],
     }
